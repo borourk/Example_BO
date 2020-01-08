607125bd13ecfeb3c7b2d130f6ef8107582950cd
Benjamin O'Rourk <benjaminorourk@benjamins-mbp.home>
2020-01-07 20:12:19 -0500

    Initial commit

---
+++ src/TestProgram.java
@@ -0,0 +1,26 @@
+
+public class TestProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*	
+		System.out.println("Hello World");
+		
+		int a = 10;
+		double g = 36.54;
+		String temp = "Interesting Factoid";
+		
+		System.out.printf("This is an integer %d, this is a double %.2f, this is a String %s%n", a, g, temp);
+		*/
+		
+		Dog d = new Dog();
+		Dog fido = new Dog("fido", 13);
+		
+		System.out.println(d.toString());
+		
+		System.out.println(fido.toString());
+			
+				
+	}
+
+}
